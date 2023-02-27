9b08dc4aef3fa0595d8d29ef7981f5c914c57e93
Raphael Abdiche <abdiche.raphael@gmail.com>
2023-02-27 20:14:54 +0100

    Init du projet ok, dépendances ok, creation des composants header, footer, maj index.js en reliant le DOM sur element root

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// const router = createBrowserRouter([
+//     {
+//         path: "/Kasa/",
+//         element: <Home />
+//     },
+//     {
+//         path: "/logement/:id",
+//         element: <Logement />
+//     },
+//     {
+//         path: '/about',
+//         element: <About />
+//     },
+//     {
+//         path: "*",
+//         element: <Error />
+//     },
+// ]);
 
-export default App;
+// function App() {
+//         return (
+//           <>
+//             <RouterProvider router={router}/>
+//           </>
+//         );
+// }
+
+// export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import Logo from '../../assets/logo.png'
+import Navbar from '../navbar/Navbar'
+
+export default function Header() {
+	return (
+		<header className='header'>
+			<h1>
+				<img src={Logo} alt="kasa, location d'appartements" />
+			</h1>
+			<Navbar />
+		</header>
+	)
+}
