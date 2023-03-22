ba8acc0991694086e69cc6159d558cbb0a049a49
Raphael Abdiche <abdiche.raphael@gmail.com>
2023-03-22 22:31:22 +0100

    Réecriture de la page Logement.js qui faisait une boucle infinie dans la gestion de l'id , modification de la condition et ajout d'un opération de vérif : ? , ensemble ok, route ok

--- src/pages/logement/Logement.js
+++ src/pages/logement/Logement.js
@@ -12,82 +12,81 @@ import greyStar from '../../assets/grey_star.png'
 
 export default function Logement() {
 
-	// Ajout du hook useNavigate qui permettra de vérifier plus tard l'existence de l'ID
+	// Utilisation du hook useNavigate pour la gestion de la navigation
 	const navigate = useNavigate();
 
-	// on définit l'état initial de imageSlider avec le hook useState qui sera un tableau vide
+	// Utilisation de useState pour stocker les images du slider
 	const [imageSlider, setImageSlider] = useState([]);
 
-     // on utilise le hook useParams pour récupérer l'id du logement et renvoyer un objet contenant les paramètres d'URL
-	// et méthode filter pour filtrer datas
-	const idLogement = useParams('id').id;
-	const dataCurrentLogement = datas.filter(data => data.id === idLogement);
-	
-     // useEffect qui sera utilisée pour exécuter l'action lorsqu'un changement est détecté dans l'id du logement
-	// si logement introuvable on renvoie aucun élement et appelle la méthode 'navigate'
+	// Utilisation du hook useParams pour atteindre l'ID du logement séléctionner dans l'URL
+	const { id } = useParams();
+
+	// Triage les données pour récupérer le logement qui correspond à l'ID
+	const dataCurrentLogement = datas.find(data => data.id === id);
+  
 	useEffect(() => {
-		const dataCurrentLogement = datas.filter(data => data.id === idLogement);
-		if (dataCurrentLogement.length === 0) {
+		// SI logement non trouver, rediriger vers la page d'erreur
+		if (!dataCurrentLogement) {
 			navigate('/error', { replace: true });
-		   } else {
-			setImageSlider(dataCurrentLogement[0].pictures);
-		   }
-		 }, [idLogement, navigate]);
+		// SINON, le state est mis à jour avec les images du slider
+		} else {
+			setImageSlider(dataCurrentLogement.pictures);
+		}
+	}, [dataCurrentLogement, navigate]);
 
-		 // on utilise dataCurrentLogement pour accèder à l'objet et aux propriétées
-		 const name = dataCurrentLogement[0].host.name.split(' '); 
-		 const rating = dataCurrentLogement[0].rating;
-		 const description  = dataCurrentLogement[0].description;
-		 const equipments = dataCurrentLogement[0].equipments;
+	const name = dataCurrentLogement?.host?.name?.split(' ') ?? [];
+	const rating = dataCurrentLogement?.rating ?? 0;
+	const description = dataCurrentLogement?.description ?? "";
+	const equipments = dataCurrentLogement?.equipments ?? [];
 
-     // on return en front les composants Header, Footer, Slider avec imageSlider afin d'afficher le carousel d'images
-	// et l'élément main contiendra les informations sur le logement
+	// Et on return en front les composants suivants :
+	// Header, Slider, les données correspondant à l'ID et le footer
 	return (
-		<>
-			<Header/>
-			<Slider imageSlider={imageSlider}/>
-				<main className="Logement">
-					<div className="Logement_content">
-						<div className="Logement_content_infos">
-							<h1>{dataCurrentLogement[0].title}</h1>
-							<p>{dataCurrentLogement[0].location}</p>
-							<div>
-								{dataCurrentLogement[0].tags.map((tag, index) => {
-									return (
-										<button key={index}>{tag}</button>
-									)
-								})}
-							</div>
-						</div>
-						<div className="Logement_content_host">
-							<div>
-								<div className='Logement_content_host_name'>
-									<span>{name[0]}</span>
-									<span>{name[1]}</span>
-								</div>
-								<img src={dataCurrentLogement[0].host.picture} alt="host of this house" />
-							</div>
-								
-							<div className="Logement_content_host_stars">
-								{[...Array(5)].map((star, index) => {
-									const ratingValue = index + 1;
-									return (
-										<img key={index} src={ratingValue <= rating ? redStar : greyStar} alt="star" />
-									)
-								})}
-							</div>
-						</div>
-					</div>
-					<div className="Logement_collapse">
-						<div className="Logement_collapse_item">
-							<Collapse title={'Description'} content={description} />	
-						</div>
-						<div className="Logement_collapse_item">
-							<Collapse title={'Équipements'} content={equipments}/>
-						</div>	
-					</div>
-				</main>
-               <Footer />
-          </>
-     )
-}
+	<>
+		<Header />
+		<Slider imageSlider={imageSlider} />
+		<main className="Logement">
+		<div className="Logement_content">
+			<div className="Logement_content_infos">
+			<h1>{dataCurrentLogement?.title}</h1>
+			<p>{dataCurrentLogement?.location}</p>
+			<div>
+			{dataCurrentLogement?.tags?.map((tag, index) => {
+				return (
+				<button key={index}>{tag}</button>
+				)
+			})}
+			</div>
+			</div>
+			<div className="Logement_content_host">
+			<div>
+			<div className='Logement_content_host_name'>
+				<span>{name[0]}</span>
+				<span>{name[1]}</span>
+			</div>
+			<img src={dataCurrentLogement?.host?.picture} alt="host of this house" />
+			</div>
+
+			<div className="Logement_content_host_stars">
+			{[...Array(5)].map((star, index) => {
+				const ratingValue = index + 1;
+				return (
+				<img key={index} src={ratingValue <= rating ? redStar : greyStar} alt="star" />
+				)
+			})}
+			</div>
+			</div>
+		</div>
+		<div className="Logement_collapse">
+			<div className="Logement_collapse_item">
+			<Collapse title={'Description'} content={description} />
+			</div>
+			<div className="Logement_collapse_item">
+			<Collapse title={'Équipements'} content={equipments} />
+			</div>
+		</div>
+		</main>
+		<Footer />
+	</>
+	)
+	}
